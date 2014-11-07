ba47e7c57c2b62378512f1b3e533904e837964ea
Mike <mmelvin0@gmail.com>
2014-11-06 20:50:36 -0800

    Update Forge to 1.7.10-10.13.2.1235

--- server/run.bat
+++ server/run.bat
@@ -1,1 +1,1 @@
-@java -jar -XX:MaxPermSize=256M -Xincgc -jar forge-1.7.10-10.13.2.1232-universal.jar nogui
+@java -jar -XX:MaxPermSize=256M -Xincgc -jar forge-1.7.10-10.13.2.1235-universal.jar nogui
