de11ac2547ca3507fb89b88b2e0681781823aea5
Mike <mmelvin0@gmail.com>
2014-11-09 16:30:07 -0800

    Downgrade Forge to 1.7.10-10.13.2.1230

--- server/run.bat
+++ server/run.bat
@@ -1,1 +1,1 @@
-@java -jar -XX:MaxPermSize=256M -Xincgc -jar forge-1.7.10-10.13.2.1235-universal.jar nogui
+@java -jar -XX:MaxPermSize=256M -Xincgc -jar forge-1.7.10-10.13.2.1230-universal.jar nogui
--- server/run.sh
+++ server/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec java -jar -XX:MaxPermSize=256M -Xincgc -jar forge-1.7.10-10.13.2.1235-universal.jar nogui
+exec java -jar -XX:MaxPermSize=256M -Xincgc -jar forge-1.7.10-10.13.2.1230-universal.jar nogui
