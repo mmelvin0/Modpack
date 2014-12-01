50760d1f642cddf739f051cd3f53b0ced15e00b4
Mike <mmelvin0@gmail.com>
2014-11-30 16:48:43 -0800

    Bump server max heap space to 3 GB

--- server/run.sh
+++ server/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec java -jar -XX:MaxPermSize=256M -Xincgc -jar forge-1.7.10-10.13.2.1230-universal.jar nogui
+exec java -jar -XX:MaxPermSize=256M -Xincgc -Xmx3g -jar forge-1.7.10-10.13.2.1230-universal.jar nogui
