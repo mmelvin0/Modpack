7d8da6467727b1487a0bf9c3fffd73f4cefbae81
Mike <mmelvin0@gmail.com>
2015-03-02 20:20:35 -0800

    Consolidate INpureCore NEI culling

--- common/config/INpureProjects/custom_nei_filters/vanilla.js
+++
@@ -1,8 +0,0 @@
-var vanilla_blocks = ["portal", "end_portal", "fire"];
-if (vanilla_enabled){
-    for each (block in vanilla_blocks){
-        // Vanilla items and blocks have special handlers.
-	    NEI.hide("minecraft", block);
-    }
-	NEI.override("minecraft:potion", [0]);
-}
