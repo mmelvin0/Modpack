aac43ad9d24a3163eb6d0bd19eaef319d327ee7e
Mike <mmelvin0@gmail.com>
2015-03-03 21:08:37 -0800

    Refine NEI culling

--- common/config/INpureProjects/custom_nei_filters/nei-culling.js
+++ common/config/INpureProjects/custom_nei_filters/nei-culling.js
@@ -1,13 +1,15 @@
+// facades and microblocks
 BC.obliterate_facades(0);
-ForgeMicroblock.obliterate_microblocks([1, 2, 4, 257, 258, 260, 513, 514, 516, 769, 770, 772], ForgeMicroblock.getRandomMaterial());
+ForgeMicroblock.obliterate_microblocks([1, 2, 4, 257, 258, 260, 513, 514, 516, 769, 770, 772], 'tile.stone');
+NEI.override(AE2.getFacadeItem(), [0]);
 
+// liquid containers
+NEI.override('extracells:pattern.fluid', [0]);
+NEI.override('IC2:itemFluidCell', [0]);
+NEI.override('Mekanism:GasTank', [100]);
+NEI.override('ThermalExpansion:florb', [0, 1]);
+
+// bibliocraft
+NEI.override('BiblioCraft:*', [0]);
 NEI.hide('BiblioWoodsBoP:*');
 NEI.hide('BiblioWoodsForestry:*');
-NEI.hide('minecraft:end_portal');
-NEI.hide('minecraft:fire');
-NEI.hide('minecraft:portal');
-NEI.override('BiblioCraft:*', [0]);
-NEI.override('Mekanism:GasTank', [100]);
-NEI.override('minecraft:potion', [0]);
-NEI.override('ThermalExpansion:florb', [0, 1]);
-NEI.override(AE2.getFacadeItem(), [java.random(AE2.getNumberOfTypes())]);
