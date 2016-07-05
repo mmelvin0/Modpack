c631dc4d9dc73790f734025ba5a2bf3affd0cddd
Mike <mmelvin0@gmail.com>
2016-07-04 17:35:16 -0700

    Add Extra Utilities

--- common/config/INpureProjects/custom_nei_filters/nei-culling.js
+++ common/config/INpureProjects/custom_nei_filters/nei-culling.js
@@ -1,10 +1,12 @@
 // facades and microblocks
 BC.obliterate_facades(0);
+ExtraUtilities.obliterate_microblocks([0, 1, 2, 3], 'tile.stone');
 ForgeMicroblock.obliterate_microblocks([1, 2, 4, 257, 258, 260, 513, 514, 516, 769, 770, 772], 'tile.stone');
 NEI.override(AE2.getFacadeItem(), [0]);
 
 // liquid containers
 NEI.override('extracells:pattern.fluid', [0]);
+NEI.override("ExtraUtilities:drum", [0, 1]);
 NEI.override('IC2:itemFluidCell', [0]);
 NEI.override('Mekanism:GasTank', [100]);
 NEI.override('ThermalExpansion:florb', [0, 1]);
